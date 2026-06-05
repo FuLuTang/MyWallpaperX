25eb87a30a770dea9c7b1160b2b06aa7118d0796
Qiufengqing <719897632@qq.com>
2026-06-05 20:32:24 +0800

    Fix Steam Workshop detail layout constraints

--- MyWallpaperX/Modules/SteamWorkshop/UI/SteamWorkshopItemDetailSheet.swift
+++ MyWallpaperX/Modules/SteamWorkshop/UI/SteamWorkshopItemDetailSheet.swift
@@ -177,7 +177,7 @@ final class AppKitSteamWorkshopItemDetailView: NSView {
         contentStack.orientation = .vertical
         contentStack.alignment = .leading
         contentStack.spacing = Metrics.contentSpacing
-        contentStack.edgeInsets = NSEdgeInsets(top: 10, left: 2, bottom: 0, right: 2)
+        contentStack.edgeInsets = NSEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
         contentStack.translatesAutoresizingMaskIntoConstraints = false
 
         documentContainer.translatesAutoresizingMaskIntoConstraints = false
